74d452e04aaad9a18ca262549c34c97850d2d9d7
EAshitey <eaashitey39@gmail.com>
2024-01-30 15:19:34 +0000

    add color constants

---
+++ lib/shared/color_constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/widgets.dart';
+
+class ColorConstants {
+  static const Color kprimary = Color(0xFFf2cd4c);
+  static const Color kbackground = Color(0xFFeeeeee);
+  static const Color kbackground2 = Color(0xFFf6f6f6);
+}
