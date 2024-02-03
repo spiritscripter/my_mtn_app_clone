780aeced0b199a95f68c4b455e97403ba94c455e
EAshitey <eaashitey39@gmail.com>
2024-02-03 11:49:53 +0000

    remove body section component keep code in home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,15 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:my_mtn_app/helpers/size_config.dart';
 import 'package:my_mtn_app/helpers/sizedbox_ext.dart';
+import 'package:my_mtn_app/shared/app_background.dart';
 
 import 'components/body_section.dart';
-import 'components/top_section.dart';
+import 'components/header.dart';
+import 'widgets/balance_section.dart';
+import 'widgets/quick_access_section.dart';
 
+@RoutePage()
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
@@ -13,13 +18,32 @@ class HomeScreen extends StatelessWidget {
     ScreenSize().init(context);
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            10.height,
-            const TopSection(),
-            const Spacer(),
-            const BodySection(),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              10.height,
+              const Header(),
+              AppBackground(
+                bottomLayer: const QuickAccessSection(),
+                upperLayer: Column(
+                  children: [
+                    20.height,
+                    const BalanceSection(),
+                    40.height,
+                    Text(
+                      "Showing balances as at Jan 30 2024; 2:42:34 PM",
+                      style: TextStyle(
+                        fontSize: 13.h,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    20.height,
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
