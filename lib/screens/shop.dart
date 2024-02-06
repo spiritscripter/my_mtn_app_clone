9f057083db25c8672e75d0f1d14ca16ce3fe673b
EAshitey <eaashitey39@gmail.com>
2024-02-06 13:12:48 +0000

    add shop screen

---
+++ lib/screens/shop.dart
@@ -0,0 +1,104 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:my_mtn_app/helpers/helpers_export.dart';
+import 'package:my_mtn_app/screens/widgets/offers.dart';
+import 'package:my_mtn_app/screens/widgets/reusable_widgets.dart';
+import 'package:my_mtn_app/shared/color_constants.dart';
+
+import 'components/header.dart';
+
+@RoutePage()
+class ShopScreen extends StatelessWidget {
+  const ShopScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        color: ColorConstants.kbackground2,
+        child: Column(
+          children: [
+            const Header(text: 'Shop'),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Container(
+                      color: ColorConstants.kprimary,
+                      height: 150.h,
+                    ),
+                    20.height,
+                    Container(
+                      width: double.infinity,
+                      padding: EdgeInsets.symmetric(horizontal: 20.w),
+                      color: ColorConstants.kbackground2,
+                      child: Column(
+                        crossAxisAlignment: crossStart,
+                        children: [
+                          buildTextHeader("What do you need?"),
+                          10.height,
+                          GridView.builder(
+                            itemCount: 8,
+                            shrinkWrap: true,
+                            padding: EdgeInsets.zero,
+                            physics: const NeverScrollableScrollPhysics(),
+                            gridDelegate:
+                                SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 3,
+                              childAspectRatio: 1.6,
+                              crossAxisSpacing: 10.w,
+                              mainAxisSpacing: 10.h,
+                            ),
+                            itemBuilder: (_, __) {
+                              return Column(
+                                // mainAxisSize: mainMin,
+                                children: [
+                                  const CircleAvatar(
+                                    radius: 20,
+                                    backgroundColor: Colors.black,
+                                    child: Icon(
+                                      Icons.network_cell,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  5.height,
+                                  Text(
+                                    'Caller Tunez',
+                                    style: TextStyle(
+                                      fontSize: 15.h,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                ],
+                              );
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 20.w),
+                      decoration: const BoxDecoration(
+                        color: ColorConstants.kbackground,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(15),
+                        ),
+                      ),
+                      child: Column(
+                        children: [
+                          50.height,
+                          const Offers(headerText: 'Pulse Offers and Loyalty'),
+                          100.height,
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
