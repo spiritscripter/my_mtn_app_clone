8f4ebd8afddb38fe424df7a34dfc9e8acbb74097
EAshitey <eaashitey39@gmail.com>
2024-02-06 12:46:12 +0000

    add offers banner

---
+++ lib/screens/widgets/offers.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:my_mtn_app/helpers/helpers_export.dart';
+import 'package:my_mtn_app/screens/widgets/reusable_widgets.dart';
+import 'package:my_mtn_app/shared/color_constants.dart';
+
+class Offers extends StatelessWidget {
+  final String headerText;
+  const Offers({
+    super.key,
+    required this.headerText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: crossStart,
+      children: [
+        buildTextHeader(headerText),
+        20.height,
+        Container(
+          height: 100.h,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: ColorConstants.kprimary,
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+      ],
+    );
+  }
+}
