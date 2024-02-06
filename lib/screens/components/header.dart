eb02cabb6b3a37a2526a6713c740e129b62bee76
EAshitey <eaashitey39@gmail.com>
2024-02-06 12:42:31 +0000

    make dynamic header for all screens

--- lib/screens/components/header.dart
+++ lib/screens/components/header.dart
@@ -1,44 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:my_mtn_app/shared/color_constants.dart';
 
 import '../../helpers/size_config.dart';
 
 class Header extends StatelessWidget {
-  const Header({
-    super.key,
-  });
+  final bool isHome;
+  final String? text;
+  const Header({super.key, this.isHome = false, this.text});
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: mainSpaceBetween,
-      children: [
-        Image.asset(
-          'assets/logo.png',
-          height: 50.h,
-          width: 50.w,
-        ),
-        Text.rich(
-          TextSpan(
-            text: 'Yo!',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 20.h,
-              fontWeight: FontWeight.bold,
+    return Container(
+      color: ColorConstants.kprimary,
+      child: Row(
+        mainAxisAlignment: mainSpaceBetween,
+        children: [
+          if (isHome) ...[
+            Image.asset(
+              'assets/logo.png',
+              height: 50.h,
+              width: 50.w,
             ),
-            children: [
+            Text.rich(
               TextSpan(
-                text: ' MTN GHANA',
+                text: 'Yo!',
                 style: TextStyle(
                   color: Colors.black,
                   fontSize: 20.h,
-                  fontWeight: FontWeight.w400,
+                  fontWeight: FontWeight.bold,
                 ),
+                children: [
+                  TextSpan(
+                    text: ' MTN GHANA',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20.h,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ),
-        IconButton(onPressed: () {}, icon: const Icon(Icons.boy_outlined)),
-      ],
+            ),
+            IconButton(
+                onPressed: () {}, icon: const Icon(Icons.child_care_outlined)),
+          ]
+
+          // Add more widgets here
+          else
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                text ?? '',
+                style: TextStyle(fontSize: 20.h, fontWeight: FontWeight.w600),
+              ),
+            ),
+        ],
+      ),
     );
   }
 }
