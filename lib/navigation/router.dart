cb68b60c62e2498df93d8e83ce6384cbcf2c5433
EAshitey <eaashitey39@gmail.com>
2024-02-06 12:41:49 +0000

    set all routes

---
+++ lib/navigation/router.dart
@@ -0,0 +1,25 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:my_mtn_app/screens/home.dart';
+import 'package:my_mtn_app/screens/momo.dart';
+import 'package:my_mtn_app/screens/more.dart';
+import 'package:my_mtn_app/screens/shop.dart';
+import 'package:my_mtn_app/screens/main_bottom_bar_screen.dart';
+
+part 'router.gr.dart';
+
+@AutoRouterConfig()
+class AppRouter extends _$AppRouter {
+  @override
+  List<AutoRoute> get routes => [
+        AutoRoute(
+          page: MainBottomBarRoute.page,
+          initial: true,
+          children: [
+            AutoRoute(page: HomeRoute.page),
+            AutoRoute(page: ShopRoute.page),
+            AutoRoute(page: MomoRoute.page),
+            AutoRoute(page: MoreRoute.page),
+          ],
+        ),
+      ];
+}
